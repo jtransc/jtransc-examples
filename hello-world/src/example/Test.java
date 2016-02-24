46d7efcbc6e4a2eac95d1f36851cc61f4bac812b
Carlos Ballesteros Velasco <soywiz@gmail.com>
2016-02-24 15:27:53 +0100

    Updated to 0.1.0

--- hello-world/src/example/Test.java
+++ hello-world/src/example/Test.java
@@ -1,7 +1,10 @@
 package example;
 
+import jtransc.JTranscSystem;
+import jtransc.JTranscVersion;
+
 public class Test {
     static public void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! " + JTranscVersion.getVersion() + ", " + JTranscVersion.getRuntime() + ", " + JTranscSystem.usingJTransc());
     }
 }
--- tic-tac-toe-kotlin/src/com/jtransc/examples/tictactoe/main.kt
+++ tic-tac-toe-kotlin/src/com/jtransc/examples/tictactoe/main.kt
@@ -4,13 +4,14 @@ import jtransc.game.JTranscGame
 
 object TicTacToeGdx {
 	@JvmStatic fun main(args: Array<String>) {
-		JTranscLibgdx.config();
+		JTranscLibgdx.init()
 		TicTacToe.main(args)
 	}
 }
 
 object TicTacToeTransc {
 	@JvmStatic fun main(args: Array<String>) {
+		JTranscLime.init()
 		TicTacToe.main(args)
 	}
 }
